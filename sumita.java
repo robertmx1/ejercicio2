c313e4f4f2a3560d69f327999770168d5479c854
robertmx1 <robert_mx1@hotmail.com>
2018-09-20 19:18:23 +0700

    la suma

---
+++ sumita.java
@@ -0,0 +1,15 @@
+public class operaciones
+{
+    public static void main (String args)
+    {
+        int x;
+        int y;
+        
+    }
+    public int suma (int a , int b)
+    {
+        int res;
+        res=a+b;
+        return res;
+    }
+}
